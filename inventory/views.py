cd4001630b09cc7d73a54b1102237e52cf965f2a
josephpro21 <elasujoseph46@gmail.com>
2026-02-15 00:30:33 +0300

    added user crud endpoint, stock transfer, storeassignment

--- inventory/views.py
+++ inventory/views.py
@@ -1,9 +1,13 @@
-from .models import StoreInventory
+from .models import StoreInventory, StockTransfer
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializers import StoreInventorySerializer
+from .serializers import StoreInventorySerializer, StockTransferSerializer
 
 # Create your views here.
 class StoreInventoryViewSet(ModelViewSet):
     queryset = StoreInventory.objects.all()
     serializer_class = StoreInventorySerializer
+
+class  StockTransferViewSet(ModelViewSet):
+    queryset = StockTransfer.objects.all()
+    serializer_class = StockTransferSerializer
