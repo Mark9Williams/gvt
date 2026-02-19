6be5bc9f042042bbfda537826b41984bf1fbe6ec
Mark9Williams <mawejjemarkwilliam@gmail.com>
2026-02-19 23:18:23 +0300

    database configuration

--- alpha/settings.py
+++ alpha/settings.py
@@ -102,7 +102,9 @@ WSGI_APPLICATION = 'alpha.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default= 'sqlite:///db.sqlite3'
+        default= 'sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
     ),
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
