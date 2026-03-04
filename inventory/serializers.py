2a258652fbc20e6bbaa1fee076f3b5484833f269
josephpro21 <elasujoseph46@gmail.com>
2026-03-04 22:58:12 +0300

    patch stock transfer

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,47 +1,47 @@
 from django.db import transaction
 from rest_framework import serializers
-from .models import StoreInventory,StockTransfer
+from .models import StoreInventory, StockTransfer
+
 
 class StoreInventorySerializer(serializers.ModelSerializer):
     class Meta:
         model = StoreInventory
-        fields = '__all__'
-
+        fields = "__all__"
 
 
 class StockTransferSerializer(serializers.ModelSerializer):
     class Meta:
-            model = StockTransfer
-            fields = "__all__"
-            #read_only_fields = ["created_by", "created_at"]
-    # def create(self, validated_data):
-    #         validated_data["created_by"] = self.context["request"].user
-    #         return super().create(validated_data)
+        model = StockTransfer
+        fields = "__all__"
+
     def create(self, validated_data):
-        user = self.context["request"].user
         with transaction.atomic():
-            transfer = StockTransfer.objects.create(
-                #created_by=user,
-                **validated_data
-            )
+            transfer = StockTransfer.objects.create(**validated_data)
+
             product = transfer.product
             quantity = transfer.quantity
+
             # Deduct from source store (if exists)
-            if transfer.from_store:
+            if transfer.source:
                 source_inventory = StoreInventory.objects.select_for_update().get(
-                store=transfer.from_store,
-                product=product
+                    store=transfer.source,
+                    product=product
                 )
+
                 if source_inventory.quantity < quantity:
                     raise serializers.ValidationError("Insufficient source stock")
+
                 source_inventory.quantity -= quantity
                 source_inventory.save()
+
             # Add to destination store
-            dest_inventory = StoreInventory.objects.select_for_update().get(
-            store=transfer.to_store,
-            product=product,
-            defaults={"quantity": 0}
+            dest_inventory = StoreInventory.objects.select_for_update().get_or_create(
+                store=transfer.destin,
+                product=product,
+                defaults={"quantity": 0}
             )
+
             dest_inventory.quantity += quantity
             dest_inventory.save()
+
         return transfer
